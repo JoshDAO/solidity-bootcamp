0e603c2e8b4267e9e8c151287f4f7fda5a34b3bd
"Joshua Hart" <"joshua.hart@protonmail.com">
2021-12-04 22:36:28 +0000

    homework 4

--- contracts/VolcanoCoin.sol
+++ contracts/VolcanoCoin.sol
@@ -3,17 +3,27 @@
 pragma solidity ^0.8.0;
 
 contract VolcanoCoin {
+
     uint totalSupply = 10000;
     address owner;
+    mapping(address => uint) public balance;
+    mapping(address => Payment[]) public userPayments;
+
+    struct Payment {
+        uint amount;
+        address recipient;
+    }
 
     constructor() {
         owner = msg.sender;
+        balance[owner] = totalSupply;
     }
 
-    event NewSupply(uint _newSupply);
+    event NewSupply(uint);
+    event Transfer(address indexed, uint);
 
     modifier onlyOwner() {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "You must be the owner.");
         _;
     }
 
@@ -24,6 +34,19 @@ contract VolcanoCoin {
     function increaseTotalSupply() public onlyOwner {
         totalSupply += 1000;
         emit NewSupply(totalSupply);
+    }
+
+    function transfer(address _recipient, uint _amount) public {
+        require(balance[msg.sender] >= _amount, "You do not have sufficient funds.");
+        require(_amount > 0, "amount must be greater than 0");
+        balance[msg.sender] -= _amount;
+        balance[_recipient] += _amount;
+        Payment memory newPayment;
+        newPayment.amount = _amount;
+        newPayment.recipient = _recipient;
+        userPayments[msg.sender].push(newPayment);
+
+        emit Transfer(_recipient, _amount);
     }
 
 
